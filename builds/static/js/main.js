4ad25d8e45735a0262266e07b21f9bed62385140
System Administrator <root@MacBook-Pro-Ruslan.local>
2015-06-30 10:03:44 +0500

    fix vk

---
+++ builds/static/js/main.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $('.modal').hide();
+
+    $('a[data="click-modal"]').click(function () {
+        var id = '';
+        id = $(this).attr('data-item');
+
+        $(id).slideDown(300);
+        $('.modal-overlay').show();
+    });
+
+    $('.modal-overlay').click(function () {
+        $('.modal, .modal-overlay').hide();
+    });
+});
